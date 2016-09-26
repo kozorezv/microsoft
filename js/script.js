2db21ba027920ddf57a276dfc2a247a940645721
vosktech <kosoresv@mail.ru>
2016-09-26 15:41:38 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+/* openning humbeurger menu */
+$(document).ready(function(){
+	$('#nav-icon1').click(function(){
+		$(this).toggleClass('open');
+		$('nav').toggleClass('displaied');
+	});
+});
+/* end of */
+
+/* smooth scrolling from menu */
+$(document).ready(function(){
+    $("#menu").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
+/* end of */
+
+/* toggling class between menu items */
+$(document).ready(function(){
+	$('.default').click(function(){
+		$('.menu-active').removeClass('menu-active').addClass('default');
+		$(this).addClass('menu-active');
+	});
+});
+/* end of */  
